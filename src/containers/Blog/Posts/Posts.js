19335e1d4325a4ceb5059928a8cba0dcc19b0664
Igor Szczerbicki <pl4cek@outlook.com>
2018-08-02 16:57:43 +0200

    Przekazywanie parametrów w URL

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import axios from "../../../axios";
 import Post from '../../../components/Post/Post'
 import './Posts.css'
+import {Link} from 'react-router-dom'
 
 class Posts extends Component {
 
@@ -34,11 +35,18 @@ class Posts extends Component {
 		let posts = <p style = {{textAlign: 'center', color: 'red'}}> Ups.. Error! </p>;
 		if (!this.state.error) {
 			posts = this.state.posts.map(post => {
-					return <Post key={post.id}
-					             title={post.title}
-					             author={post.author}
-					             clicked={() => this.postSelectedHandler(post.id)}
-					/>
+					return (
+						<Link
+							key={post.id}
+							to={'/' + post.id}
+						>
+							<Post
+								title={post.title}
+								author={post.author}
+								clicked={() => this.postSelectedHandler(post.id)}
+							/>
+						</Link>
+					)
 				}
 			);
 		}
