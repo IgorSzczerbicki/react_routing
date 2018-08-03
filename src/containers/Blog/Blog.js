0db7d799dfc0de536a61a22ba60f68d549f6daf2
Igor Szczerbicki <pl4cek@outlook.com>
2018-08-03 09:26:34 +0200

    Navigation Guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,10 @@ import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
 
+	state = {
+		auth: false
+	};
+
 	render () {
 		return (
 			<div className="Blog">
@@ -40,7 +44,7 @@ class Blog extends Component {
 					</nav>
 				</header>
 				<Switch>
-					<Route path = "/new-post" component = {NewPost}/>
+					{ this.state.auth ? <Route path = "/new-post" component = {NewPost}/> : null }
 					<Route path = "/posts" component = {Posts}/>
 					<Redirect from='/' to='/posts'/>
 				</Switch>
